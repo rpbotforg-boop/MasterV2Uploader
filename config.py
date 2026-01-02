064a349c363b5f06b791f636f044ff61533bdf7a
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-03 00:42:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("")
-    API_ID = int(os.environ.get(""))
-    API_HASH = os.environ.get("")
+    BOT_TOKEN = os.environ.get("8225118430:AAEOFTRcVH3Au1LR0iFyasUb4U5CGATuoT4")
+    API_ID = int(os.environ.get("22447622"))
+    API_HASH = os.environ.get("543b62d58d3e723e766ba57a984ab65d")
     VIP_USER = os.environ.get('VIP_USERS', '').split(',')
     VIP_USERS = [int() for user_id in VIP_USER]
